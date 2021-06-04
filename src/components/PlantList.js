418be91e84253c81f0f867e27e62f465e2c2f695
Brian Nunez <nunez27@brians-mbp.myfiosgateway.com>
2021-06-04 19:14:28 -0400

    All done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,6 +4,14 @@ import axios from "axios";
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
 
+  constructor() {
+    super();
+    this.state ={
+      plants: []
+    };
+  }
+    
+
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -12,8 +20,9 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
-      });
+        this.setState({plants: res.data})
+      }); 
+      // .then(res => this.setState({plants: res.data}));
   }
 
   render() {
